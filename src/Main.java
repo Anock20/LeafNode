7bf475b36adb171c470438410df0c1b67cad1652
Anock20 <119288779+Anock20@users.noreply.github.com>
2023-05-22 21:06:04 +0900

    1

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,25 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Main {
     static int[] parent;
     static int count;
+
     public static void main(String[] args) {
         test();
     }
+
     public static void test() {
         count = 0;
         parent = new int[]{-1, 0, 0, 1, 1};
         deleteNode(4);
         countLeaf(getRoot(), new boolean[parent.length]);
         System.out.println(count);
+
         count = 0;
         parent = new int[]{-1, 0, 0, 1, 1, 1, 2};
         deleteNode(1);
         countLeaf(getRoot(), new boolean[parent.length]);
         System.out.println(count);
     }
+
     public static int getRoot() {
         int root = 0;
         for (int i = 0; i < parent.length; i++) {
@@ -28,12 +28,22 @@ public class Main {
         }
         return root;
     }
-    public static void deleteNode(int d){
-        parent[d] = -2;
 
+    public static void deleteNode(int d) {
+        parent[d] = -2; // 삭제한 노드는 -2
     }
-    public static void countLeaf(int s, boolean[] visited){
 
-
+    public static void countLeaf(int s, boolean[] visited) {
+        visited[s] = true;
+        boolean leafcheck = true;
+        for (int i = 0; i < parent.length; i++) {
+            if (parent[i] == s && !visited[i]) { // 방문하지 않은 자식 노드가 있는 경우 해당 자식 노드로 재귀 호출
+                leafcheck = false;      // visited 배열로 이미 확인한 노드 체크
+                countLeaf(i, visited);
+            }
+        }
+        if (leafcheck) { //리프노드인 경우 count ++
+            count++;
+        }
     }
 }
